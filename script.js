f67405b1faf6895532d43547958ef7d08f309d54
Tarun Singh <122197511+Tarun-7466@users.noreply.github.com>
2024-06-22 16:03:02 +0530

    responsive

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let circle = document.querySelector('.circle');
+// let circle = document.querySelector('.circle');
 
-document.body.addEventListener('mousemove', function (event) {
-    let xValue = event.clientX;
-    let yValue = event.clientY;
+// document.body.addEventListener('mousemove', function (event) {
+//     let xValue = event.clientX;
+//     let yValue = event.clientY;
 
-    // Update the circle's position with a slight delay for smoother motion
-    setTimeout(function () {
-        circle.style.top = `${yValue}px`;
-        circle.style.left = `${xValue}px`;
-    }, 10); // Adjust the delay (in milliseconds) as needed for smoother animation
-});
+//     // Update the circle's position with a slight delay for smoother motion
+//     setTimeout(function () {
+//         circle.style.top = `${yValue}px`;
+//         circle.style.left = `${xValue}px`;
+//     }, 10); // Adjust the delay (in milliseconds) as needed for smoother animation
+// });
+let menuButton = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    menuButton.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+   
+}
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    menuButton.classList.remove('active');
+}
